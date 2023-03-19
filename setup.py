dfc3b9231dd211025036b501965b61d5f3e8e48f
autsauce <46509848+autsauce@users.noreply.github.com>
2023-03-19 19:54:35 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 import setuptools
+import subprocess
+
+def get_git_version():
+    try:
+        version = subprocess.check_output(['git', 'describe', '--tags']).decode().strip().split('v')[1]
+    except subprocess.CalledProcessError:
+        version = '0.0.0'
+
+    return version
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="portoptpy",
-    version="0.1.0",
+    version=get_git_version(),
     author="Maddox Southard",
     author_email="maddoxsouthard@yahoo.com",
     description="A Python library for interfacing with the Portfolio Optimizer API: https://docs.portfoliooptimizer.io/",
